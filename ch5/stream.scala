4e892d8c844114220deed4ca2e7f91b93530aa26
Bo Marchman <bo.marchman@gmail.com>
2018-11-05 08:34:22 -0500

    Add zipAll

--- ch5/stream.scala
+++ ch5/stream.scala
@@ -95,6 +95,16 @@ sealed trait Stream[+A] {
       case _ => None
     })
 
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] =
+    unfold((this, s2))({
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(((Some(h1()), Some(h2())), (t1(), t2())))
+      case (Cons(h1, t1), _) => Some(((Some(h1()), None), (t1(), Empty)))
+      case (_, Cons(h2, t2)) => Some(((None, Some(h2())), (Empty, t2())))
+      case _ => None
+    })
+
+  def hasSubsequence[A](s: Stream[A]): Boolean =
+    this.zipAll(s)
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
